97fbe7eaf1cdbc37d6450e321bb2e075276b8b31
sheryyll <jenishereyl@gmail.com>
2025-02-18 15:42:56 +0530

    Add files via upload

---
+++ day2_prblm2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+int findlength(char *str){
+    int length = 0;
+    for( char *p_ch = str; (*p_ch)!= '\0';p_ch++){
+        length++;
+    }
+    return length;
+}
+void reversestring(char *str){
+    int len = findlength(str); 
+    for(int left = 0,right = len - 1; left < right; left ++, right --){
+        char temp = str[left]; //*(str + length) = str[length]
+        str[left] = str[right];
+        str[right] = temp;
+    }
+}
+
+int main(){
+    char str[255];
+    printf("Enter a string\n");
+    scanf("%s",str);
+    printf("BEFORE REVERSING: %s\n",str);
+    reversestring(str);
+    printf("AFTER REVERSING: %s\n",str);
+    return 0;
+
+}
