32bfac2d80fd359ab1ad35e7b8d2eac5f2f49e9f
sheryyll <jenishereyl@gmail.com>
2025-02-19 11:45:38 +0530

    Update codechef_applenorange.c

--- codechef_applenorange.c
+++ codechef_applenorange.c
@@ -8,7 +8,7 @@ Note that every fruit with Rushitote must be distributed, there cannot be any le
 
 For example, 
 2 apples and 4 oranges can be distributed equally to two contestants, where each one receives 1 apple and 2 oranges.
-However, 2 apples and 5 oranges can only be distributed equally to one contestant.*?
+However, 2 apples and 5 oranges can only be distributed equally to one contestant.*/
 
 #include <stdio.h>
 
